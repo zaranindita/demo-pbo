abe271f0e58cce53be01da0784d26844beb26c74
zaranindita <160976449+zaranindita@users.noreply.github.com>
2025-05-04 18:35:43 +0700

    Update Note.java

---
+++ demo/src/main/model/Note.java
@@ -0,0 +1,31 @@
+package com.kidaro.kael.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public Note() {}
+
+    public Note(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
